8f2c12211ffb311502dc7b2c70ccd248ed0ca5e2
magdalena-kuczaba <mj.kuczaba@gmail.com>
2012-07-22 21:42:03 +0200

    drugi comit

---
+++ ror-webmuse/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorWebmuse::Application.config.secret_token = 'c5caafe962f52c1d11440f198e1fa6100235b523484be0a0f6b0f6a0af01eea69899015eee5beac7e20d72e076d8448a6ab04e5abcf36e92ff3db40106b5dcd8'
