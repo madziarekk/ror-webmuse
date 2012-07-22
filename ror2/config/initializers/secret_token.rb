bf38e1d82ed50db6ea1ed734d13d426d618b6f03
magdalena-kuczaba <mj.kuczaba@gmail.com>
2012-07-22 21:46:37 +0200

    first commit

---
+++ ror2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ror2::Application.config.secret_token = 'bf940ea4c0f1af8264b6ec4fd86f65039d9d9bf54cbf1fc106b6a7dbbcbd596b2ae52e896f45092aacbb00e7cfe9a12c3e5afd6455cb98430640ba7ede89d410'
